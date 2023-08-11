960381a8e641e7a9d42e3644e8ca3c39935de21b
David <acostafajardodavid@gmail.com>
2023-08-11 00:56:09 -0700

    encrypt and decrypt passwords

--- apps/password_manager/views.py
+++ apps/password_manager/views.py
@@ -3,6 +3,8 @@ from django.views.generic import CreateView, FormView, TemplateView
 
 from .models import Password
 
+from cryptography.fernet import Fernet
+
 # Create your views here.
 
 
@@ -16,3 +18,35 @@ class Home(TemplateView):
         context = {"password": password}
 
         return render(request, self.template_name, context)
+
+
+class SavePassword(CreateView):
+    model = Password
+    
+    def post(self,request,*args,**kwargs):
+        user = request.user
+        password = request.POST.get("password")
+        title= request.POST.get("title")
+        if password and title:
+            password_encrypted = encrypt(password)
+            password_created = Password.objects.create(author=user,title=title,password=password_encrypted)
+            password_created.save()
+
+            success = {
+                'success':' Password saved successfully',
+                'title': 'title',
+                'id': password_created.id,
+            }
+
+
+def encrypt(password):
+    key = Fernet.generate_key()
+    f = Fernet(key)
+    encrypted = f.encrypt(password.encode())
+    return encrypted
+        
+def decrypt(password):
+    key = Fernet.generate_key()
+    f = Fernet(key)
+    decrypted = f.decrypt(password.encode())
+    return decrypted
